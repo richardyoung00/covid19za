4f08c77450db5916da81dc09e8081fb37c966593
Richard Young <richard.young2@ibm.com>
2021-07-13 11:19:15 +0200

    Add scrape and commit script

---
+++ scripts/scrape_vaccine_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+git checkout provincial_vaccine_data
+
+python3 sacoronavirus_provincial_vaccine.py
+
+git checkout master
+
+git add "data/covid19za_provincial_cumulative_timeline_vaccination.csv"
+
+curr_date=$(date +%Y-%m-%d)
+
+git commit -m "Scrape and update provincial vaccination data for $curr_date"
