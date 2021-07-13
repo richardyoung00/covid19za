b59a551a8b36a1dc14083dbb744537af2217246b
Richard Young <richard.young2@ibm.com>
2021-07-13 12:00:52 +0200

    Update scrape and commit script

--- scripts/scrape_vaccine_data.sh
+++ scripts/scrape_vaccine_data.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-git checkout provincial_vaccine_data
+git checkout provincial_vaccine_data || exit 1;
 
-python3 sacoronavirus_provincial_vaccine.py
+python3 sacoronavirus_provincial_vaccine.py || exit 1;
 
-git checkout master
+git add "../data/covid19za_provincial_cumulative_timeline_vaccination.csv" || exit 1;
 
-git add "data/covid19za_provincial_cumulative_timeline_vaccination.csv"
+curr_date=$(date +%Y-%m-%d) || exit 1;
 
-curr_date=$(date +%Y-%m-%d)
+git commit -m "Scrape and update provincial vaccination data for $curr_date" || exit 1;
 
-git commit -m "Scrape and update provincial vaccination data for $curr_date"
+commit_hash=`git rev-parse HEAD` || exit 1;
 
-git push
+git push || exit 1;
 
-git checkout provincial_vaccine_data
+git checkout master || exit 1;
+
+git cherry-pick $commit_hash || exit 1;
+
+git push || exit 1;
+
+git checkout provincial_vaccine_data || exit 1;
